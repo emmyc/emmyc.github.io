a74f85ea5465699e94036f3af0f3522178313433
emmyc <emmycao11@gmail.com>
2018-10-18 11:45:48 -0700

    Changed fade timing

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const sr = ScrollReveal();
 
+sr.reveal('.header-container', {
+    duration:1000
+})
+
 sr.reveal('.home-pic', {
+    delay: 200,
     duration: 1500
 });
 sr.reveal('.about-text', {
