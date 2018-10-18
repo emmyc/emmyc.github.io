df1dafe28025ae2375f67e39550fa86556426967
emmyc <emmycao11@gmail.com>
2018-10-18 00:04:20 -0700

    Added ScrollReveal.js animations

---
+++ app.js
@@ -0,0 +1,14 @@
+const sr = ScrollReveal();
+
+sr.reveal('.home-pic', {
+    duration: 1500
+});
+sr.reveal('.about-text', {
+    delay: 500,
+    duration: 1500
+});
+
+
+sr.reveal('.work-link', {
+    duration: 1500
+} );
